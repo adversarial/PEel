ea29f3de31f37af228e789ccf31d9e535a184cc2
x8esix <x8esix@live.com>
2013-09-03 18:40:31 -0700

    commenting and minor fixes

--- peel/kpeb.c
+++ peel/kpeb.c
@@ -1,3 +1,18 @@
+/*
+ * (C) Copyright 2013 x8esix.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 3.0 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
 #include "kpeb.h"
 
 void* EXPORT LIBCALL KpGetCurrentPeb() {
@@ -12,14 +27,17 @@ void* EXPORT LIBCALL KpGetCurrentPeb() {
 
 wchar_t* EXPORT LIBCALL KpGetEnvironmentVariable(IN const PEB* pPeb, IN const wchar_t* wzVar) {
 	MEMORY_BASIC_INFORMATION mbi = {0};
-	wchar_t* wzInternal = *(wchar_t**)((DWORD_PTR)pPeb->ProcessParameters + offsetof(PROCESS_PARAMETERS, Environment));
+  // there's no size variable for number of environment variables or space allocated
+  // so we have to continue through each null terminated pair until we reach the end of the allocation
+  // format: db 'Variable=Value',0,'Variable=Value',0
+	wchar_t* wzInternal = *(wchar_t**)((PTR)pPeb->ProcessParameters + offsetof(PROCESS_PARAMETERS, Environment));
 	VirtualQuery(wzInternal, &mbi, sizeof(MEMORY_BASIC_INFORMATION));
 	do {
 		if (!wcsnicmp(wzInternal, (wchar_t*)wzVar, wcslen((wchar_t*)wzVar))) {
 			return wcschr(wzInternal, L'=') + 1;
 		}
-		wzInternal = (wchar_t*)((DWORD_PTR)wzInternal + (((DWORD_PTR)wcslen(wzInternal) + 1) * sizeof(wchar_t)));	// next var
-	} while ((DWORD_PTR)wzInternal < mbi.RegionSize + (DWORD_PTR)mbi.BaseAddress);
+		wzInternal += wcslen(wzInternal) + 1;	// next var
+	} while ((PTR)wzInternal < (PTR)mbi.BaseAddress + mbi.RegionSize);
 	return NULL;
 }
 
@@ -31,14 +49,14 @@ LDR_MODULE32* EXPORT LIBCALL KpGetLdrModule(IN const wchar_t* wzName) {
     pPeb = (PEB*)KpGetCurrentPeb();
     if (pPeb == NULL)
         return NULL;
-
+  // list is circular (last flink points to header)
     plmModule = (LDR_MODULE32*)pPeb->Ldr;
     plmModuleHeader = plmModule;
     do {
-      // search for target module (either 8 or smaller name)
+      // search for target module
         if (!wcsnicmp(wzName, plmModule->BaseDllName.Buffer, wcslen(wzName)))
             return plmModule;
         plmModule = (LDR_MODULE32*)plmModule->InMemoryOrderModuleList.Flink;
-    } while (plmModule != plmModuleHeader); // circular list
+    } while (plmModule != plmModuleHeader);
     return NULL;
 }
--- peel/kpeb.h
+++ peel/kpeb.h
@@ -1,3 +1,18 @@
+/*
+ * (C) Copyright 2013 x8esix.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 3.0 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
 #pragma once
 
 #include "peel.h"
