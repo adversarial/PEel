149ca598601f1c094248658aa53925bb9ac47752
x8esix <x8esix@live.com>
2013-09-15 13:58:51 -0700

    c99 standard (moving away from legacy style code)

--- buildgcc.bat
+++ buildgcc.bat
@@ -1,3 +1,6 @@
+:: Windows build script for PEel
+:: x8esix
+
 @echo off
 
 :: gcc appears to produce MUCH smaller builds (20kb vs 175 kb)
@@ -7,23 +10,29 @@ set sources=
 set objects=
 
 :: cleanup     
-::   FOR %%i in (*.o) DO del %%i
+     FOR %%i in (*.o) DO del %%i
      IF EXIST ..\Release\PEel.lib del ..\Release\PEel.lib /Q
 
 cd .\peel\
 
      FOR %%i in (*.c) DO (call :concat %%i & gcc -c %%i -std=c99 -O2 -pedantic & ECHO Compiling %%i)
+     ECHO.
+     ECHO Linking library...
+     ECHO.
      FOR %%i in (*.o) DO (call :concat2 %%i & ECHO Adding %%i to library)
 
      ar rcs ..\Release\PEel.lib %objects%
 
+ECHO.
 ECHO Cleaning up...
 ECHO.
     
-     FOR %%i in (*.o) DO del %%i
+     FOR %%i in (*.o) DO (del %%i & ECHO Deleting %%i...)
 
+ECHO.
 ECHO Compilation complete!
 ECHO.
+pause
 
 :concat
 set sources=%sources% %1
